c2de27686aacbf3d0e49f24a613c596c42c54faf
segevitsky <segevitsky@gmail.com>
2019-05-21 17:12:11 +0300

    first commit

---
+++ models/campground.js
@@ -0,0 +1,26 @@
+var mongoose = require('mongoose');
+
+//SCHEMA SETUP
+var campgroundSchema = new mongoose.Schema({
+    name: String,
+    price: String, 
+    image: String,
+    description: String,
+    author: {
+        id:{
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User'
+        },
+        username: String
+    },
+    comments: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Comment"
+        }
+    ]
+});
+
+module.exports = mongoose.model('Campground', campgroundSchema);
+//this is going to give us methods to interact with our database 
+// like campground.find
